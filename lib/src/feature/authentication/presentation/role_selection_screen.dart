fb49a063423b97937814ac99141c14174554fb95
Rachel <rmktumulak@email.com>
2025-08-25 21:59:52 -0400

    style: separate class in a file for screen

---
+++ lib/src/feature/authentication/presentation/role_selection_screen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod_boilerplate/src/routing/business/business_router.dart';
+import 'package:flutter_riverpod_boilerplate/src/routing/clientele/clientele_router.dart';
+import 'package:go_router/go_router.dart';
+
+class RoleSelectionScreen extends StatelessWidget {
+  const RoleSelectionScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Select Role')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () => context.go(AppRoute.schedule.name),
+              child: Text('Continue as Tenant'),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () =>
+                  context.go(ClienteleRoute.clienteleBookings.name),
+              child: Text('Continue as Client'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
